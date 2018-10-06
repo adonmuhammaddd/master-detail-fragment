61a75c862fe45b6e1fa6386ef4a44da4e4401625
bangmadown <bang.madown@gmail.com>
2018-10-06 16:27:21 +0700

    first commit

---
+++ app/src/main/java/crispyid/co/id/masterdetaildemo/ItemDetailActivity.java
@@ -0,0 +1,28 @@
+package crispyid.co.id.masterdetaildemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+
+public class ItemDetailActivity extends FragmentActivity {
+	ItemDetailFragment fragmentItemDetail;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_item_detail);
+		Item item = (Item) getIntent().getSerializableExtra("item");
+		if (savedInstanceState == null) {
+			fragmentItemDetail = ItemDetailFragment.newInstance(item);
+			FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+			ft.replace(R.id.flDetailContainer, fragmentItemDetail);
+			ft.commit();
+		}
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.item_detail, menu);
+		return true;
+	}
+}
